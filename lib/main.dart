164108c254359591fe14725afa1e3e0a14c8410d
Lakkyra <lucky15nov2005@gmail.com>
2024-08-31 10:52:49 +0530

    added internet permission

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Songs',
+      title: 'SongList',
       theme: ThemeData.dark().copyWith(
-        useMaterial3: true,
+        // useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
